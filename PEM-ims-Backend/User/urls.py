bff6697b77d876abc2c1413c5072d45c423acb8f
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-11-01 23:48:11 +0545

    minor issue

--- PEM-ims-Backend/User/urls.py
+++ PEM-ims-Backend/User/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_obtain_pair'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
